77cd057daa535dfa4e63b36f87e29cac2787f6bb
Tommy-ASD <git@tommyasd.com>
2025-12-09 04:36:46 +0100

    movement API

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,10 +2,15 @@ use axum::{
     Json, Router,
     routing::{get, post},
 };
+use http::Method;
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use tower_http::cors::CorsLayer;
 
-use engine::board::{Board, Coord, fen::fen_to_board};
+use engine::board::{
+    Board, Coord, GameMove,
+    fen::{board_to_fen, fen_to_board},
+};
 
 #[derive(Debug, Deserialize)]
 pub struct GetMovesRequest {
@@ -18,17 +23,56 @@ pub struct GetMovesResponse {
     pub moves: Vec<Coord>,
 }
 
+#[axum::debug_handler]
 async fn get_moves_handler(Json(req): Json<GetMovesRequest>) -> Json<GetMovesResponse> {
     let board = fen_to_board(&req.board_fen);
-    let moves = board.get_moves(&req.from);
+    let moves = board
+        .get_moves(&req.from)
+        .iter()
+        .map(|game_move| game_move.to.clone())
+        .collect();
     Json(GetMovesResponse { moves })
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetNewBoardStateRequest {
+    pub board_fen: String,
+    pub from: Coord,
+    pub to: Coord,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetNewBoardStateResponse {
+    pub new_board_fen: String,
+}
+
+#[axum::debug_handler]
+async fn get_new_board_state_handler(
+    Json(req): Json<GetNewBoardStateRequest>,
+) -> Json<GetNewBoardStateResponse> {
+    let mut board = fen_to_board(&req.board_fen);
+    let game_move = GameMove {
+        from: req.from,
+        to: req.to,
+    };
+    board.make_move(game_move);
+    let new_board_fen = board_to_fen(&board);
+    Json(GetNewBoardStateResponse { new_board_fen })
+}
+
 pub async fn serve_api() {
+    let cors = CorsLayer::new()
+        .allow_origin("*".parse::<http::HeaderValue>().unwrap()) // allow all — dev only
+        .allow_methods([Method::GET, Method::POST])
+        .allow_headers([http::header::CONTENT_TYPE]);
+
     let port = 8080;
     let binding_address = "0.0.0.0".to_string() + ":" + &port.to_string();
 
-    let app = Router::new().route("/board/moves", post(get_moves_handler));
+    let app = Router::new()
+        .route("/board/moves", post(get_moves_handler))
+        .route("/board/new_state", post(get_new_board_state_handler))
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind(&binding_address)
         .await
